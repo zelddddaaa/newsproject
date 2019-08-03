fc88ce5144b4bb0293954f912c3978ac74cb8ea1
zelda <1930313663@qq.com>
2019-08-03 21:26:37 +0800

    搜索结果布局,渲染数据

--- src/views/search/search-result.vue
+++ src/views/search/search-result.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>{{$route.params.q}}</div>
+  <div>
+       <van-nav-bar fixed title="搜索结果" left-text="返回" left-arrow @click-left="$router.back()"></van-nav-bar>
+       <van-list class="search-list" v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+           <van-cell v-for="item in resultdata" :key="item.art_id" :title="item.title"></van-cell></van-cell>
+       </van-list>
+  </div>
 </template>
 
 <script>
@@ -10,7 +15,9 @@ export default {
   data () {
     return {
       // 搜索结果
-      resultdata: []
+      resultdata: [],
+      // 是否处于加载状态
+      loading: false
     }
   },
   created () {
@@ -30,10 +37,26 @@ export default {
       } catch (err) {
         console.dir(err)
       }
+    },
+    // list的onload
+    onload () {
+
     }
   }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.search-list {
+  margin-top: 92px;
+}
+.van-nav-bar {
+  background-color: #0096fa;
+
+  .van-nav-bar__title,
+  .van-nav-bar__text,
+  .van-icon {
+    color: #fff;
+  }
+}
 </style>
