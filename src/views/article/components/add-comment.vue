9c6175db42e7f3c45b26f8f43c16cf0766f15803
zelda <1930313663@qq.com>
2019-08-05 20:51:06 +0800

    发布评论组件结构样式

---
+++ src/views/article/components/add-comment.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="add-comment">
+        <div class="input-wrap">
+            <input type="text" v-model="content">
+        </div>
+        <div class="more-wrap">
+            <van-button size="small" :disabled="!content.length" @click="handleAdd">发布</van-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    handleAdd () {
+
+    }
+  }
+
+}
+</script>
+
+<style lang="less" scoped>
+.add-comment {
+  width: 100%;
+  background: #fff;
+  padding: 20px;
+  border-top: 2px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .input-wrap {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 20px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    width: 50%;
+    input {
+      border: none;
+      color: #999;
+      font-size: 30px;
+    }
+  }
+  .more-wrap {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+}
+
+</style>
