34c768d5a4866ea967201eef8216f3166796e1c8
zelda <1930313663@qq.com>
2019-08-06 10:35:50 +0800

    未登录组件完成

--- src/views/mine/index.vue
+++ src/views/mine/index.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>mine</div>
+    <div>
+        <!-- 未登录 使用组件带- -->
+        <un-login v-if="!$store.state.user"></un-login>
+        <!-- 登录后 -->
+        <user-info v-else></user-info>
+    </div>
 </template>
 
 <script>
+// 导入用户信息组件 帕斯卡命名法
+import UserInfo from './components/user-info.vue'
+// 导入未登录组件
+import UnLogin from './components/un-login.vue'
 export default {
+  name: 'MyIndex',
+  // 注册组件
+  components: {
+    UserInfo,
+    UnLogin
+  }
 
 }
 </script>
